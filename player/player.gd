d66bff117a16a2f974cdd4490095e8569e91cac4
Markus Hacker <markus.hacker@ezsh.de>
2025-11-06 10:06:58 +0100

    Initialer Commit nach dem Tutorial
    Basic Game is fished
    - Player ist animated and able to move
    - basic level

---
+++ player/player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+const GRAVITY: float = 8.1
+const SPEED: int = 50
+const JUMP_POWER: int = -150
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("left"):
+		velocity.x = -SPEED
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = true
+	elif Input.is_action_pressed("right"):
+		velocity.x = SPEED
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = false
+	else:
+		velocity.x = 0
+		$AnimatedSprite2D.play("idle")
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y += JUMP_POWER
+		
+	velocity.y += GRAVITY
+		
+	move_and_slide()
