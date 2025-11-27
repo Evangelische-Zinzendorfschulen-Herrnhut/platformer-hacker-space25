a12551dbfa625387b2bd9fc0d9c12a10a9fa1e87
Markus Hacker <markus.hacker@ezsh.de>
2025-11-27 11:05:24 +0100

    Revert "Cion"
    This reverts commit d9f0b0d87134244576da2d0c5ba6c9c326e4a4bb, reversing
    changes made to 4a1939b9ff088faa56ce641ac7cdc13af3a49ca1.

--- player/player.gd
+++ player/player.gd
@@ -4,32 +4,27 @@ const GRAVITY: float = 8.1
 const SPEED: int = 50
 const JUMP_POWER: int = -150
 
-@export var movment_on = true
-
+# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 func _physics_process(delta: float) -> void:
-	movment()
-
-func movment():
-	if movment_on == true:
-		if Input.is_action_pressed("left"):
-			velocity.x = -SPEED
-			$AnimatedSprite2D.play("run")
-			$AnimatedSprite2D.flip_h = true
-		elif Input.is_action_pressed("right"):
-			velocity.x = SPEED
-			$AnimatedSprite2D.play("run")
-			$AnimatedSprite2D.flip_h = false
-		else:
-			velocity.x = 0
-			$AnimatedSprite2D.play("idle")
+	if Input.is_action_pressed("left"):
+		velocity.x = -SPEED
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = true
+	elif Input.is_action_pressed("right"):
+		velocity.x = SPEED
+		$AnimatedSprite2D.play("run")
+		$AnimatedSprite2D.flip_h = false
+	else:
+		velocity.x = 0
+		$AnimatedSprite2D.play("idle")
+	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y += JUMP_POWER
 		
-		if Input.is_action_just_pressed("jump") and is_on_floor():
-			velocity.y += JUMP_POWER
-	
-			velocity.y += GRAVITY
-	
-			move_and_slide()
+	velocity.y += GRAVITY
+		
+	move_and_slide()
